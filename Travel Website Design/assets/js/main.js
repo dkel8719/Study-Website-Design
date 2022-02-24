c1cf190e4d40494198fa5f52e12acc10b0539903
Cheongsu Lee <dkel8719@naver.com>
2022-02-24 16:16:31 +0900

    scroll css/js

--- Travel Website Design/assets/js/main.js
+++ Travel Website Design/assets/js/main.js
@@ -15,6 +15,67 @@ if(navclose){
   })
 }
 
+// ================= remove menu ========================
+const navLink =document.querySelectorAll('.nav-link')
+
+navLink.forEach(link => link.addEventListener('click', () => {
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show-menu')
+}))
+
+// ===================== scroll event =========================== 
+// ===================== active link color =====================
+const sections = document.querySelectorAll('section[id]')
+
+function activeLink(){
+  const scrollY = window.pageYOffset
+
+  sections.forEach(current => {
+    const sectionTop = current.offsetTop - 1 
+    const sectionHeight = current.offsetHeight;
+    const sectionId = current.getAttribute('id')
+
+    if(scrollY >= sectionTop && scrollY < sectionTop + sectionHeight){
+      document.querySelector(`.nav-menu a[href*=${sectionId}]`).classList.add('active-link')
+    } else {
+      document.querySelector(`.nav-menu a[href*=${sectionId}]`).classList.remove('active-link')
+    }
+
+    console.log(scrollY)
+    console.log(sectionTop)
+    console.log(sectionHeight)
+    console.log(sectionId)
+  
+  })
+}
+
+window.addEventListener('scroll', activeLink)
+
+// ================= change background header ==================
+function scrollHeader(){
+  const header = document.getElementById('header')
+  
+  if(this.scrollY >= 100){
+    header.classList.add('scroll-header')
+  } else {
+    header.classList.remove('scroll-header')
+  }
+}
+
+window.addEventListener('scroll', scrollHeader)
+
+// ==================== show scroll up =======================
+function showScrollUp(){
+  const scrollUp = document.getElementById('scroll-up')
+  if(this.scrollY >= 200){
+    scrollUp.classList.add('show-scroll')
+  } else {
+    scrollUp.classList.remove('show-scroll')
+  }
+}
+
+window.addEventListener('scroll', showScrollUp)
+
 // ================== swiper ===================== 
 var swiper = new Swiper(".discover-container", {
   effect: "coverflow",
@@ -57,3 +118,5 @@ function endedVideo(){
 }
 
 videoFile.addEventListener('ended', endedVideo)
+
+
