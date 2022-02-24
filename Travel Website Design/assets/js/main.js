58b5e3b22f7db356fdaa792d6353219554c0fa1b
Cheongsu Lee <dkel8719@naver.com>
2022-02-24 16:55:00 +0900

    dark mode

--- Travel Website Design/assets/js/main.js
+++ Travel Website Design/assets/js/main.js
@@ -40,12 +40,6 @@ function activeLink(){
     } else {
       document.querySelector(`.nav-menu a[href*=${sectionId}]`).classList.remove('active-link')
     }
-
-    console.log(scrollY)
-    console.log(sectionTop)
-    console.log(sectionHeight)
-    console.log(sectionId)
-  
   })
 }
 
@@ -119,4 +113,25 @@ function endedVideo(){
 
 videoFile.addEventListener('ended', endedVideo)
 
+// ============= dark mode ====================
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
 
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+if(selectedTheme){
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click', () => {
+  document.body.classList.toggle(darkTheme)
+  themeButton.classList.toggle(iconTheme)
+  localStorage.setItem('selected-theme', getCurrentTheme())
+  localStorage.setItem('selected-icon', getCurrentIcon())
+})
